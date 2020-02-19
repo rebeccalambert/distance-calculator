f505d80b86480abc710787617d76b19e7a21cc7a
Rebecca Lambert <rebeccalambert@berkeley.edu>
2020-02-19 12:12:56 -0800

    List current City

--- imports/ui/Info.jsx
+++ imports/ui/Info.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 
-function Locations({ maxDistance, Longitude, Latitude }) {
+function Locations({ maxDistance, currCity, Longitude, Latitude }) {
   return (
     <section>
+      <p>You are in {currCity}.</p>
       <p>You want to stay within {maxDistance} miles.</p>
     </section>
   );
