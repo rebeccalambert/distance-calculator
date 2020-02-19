3ea42297ac40d06df64341a2cbb033b90f5f4e88
Rebecca Lambert <rebeccalambert@berkeley.edu>
2020-02-19 10:14:00 -0800

    Initial commit, change hello file name to InputLocation

---
+++ imports/ui/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import InputLocation from './InputLocation.jsx';
+import Info from './Info.jsx';
+
+const App = () => (
+  <div>
+    <h1>Welcome to a distance calculator!</h1>
+    <InputLocation />
+    <Info />
+  </div>
+);
+
+export default App;
