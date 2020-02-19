083d6aeb4213913a9bc5c922257a60cddf1de98f
Rebecca Lambert <rebeccalambert@berkeley.edu>
2020-02-19 10:35:23 -0800

    Move state to app.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputLocation from './InputLocation.jsx';
 import Info from './Info.jsx';
 
-const App = () => (
-  <div>
-    <h1>Welcome to a distance calculator!</h1>
-    <InputLocation />
-    <Info />
-  </div>
-);
+
+function App() {
+  const [count, setCount] = useState(["0"])
+  // state = {
+  //   counter: 0,
+  // }
+
+  const increment = () => {
+    // this.setState({
+    //   counter: this.state.counter + 1
+    // });
+    const newCount = (Number(count) + 1)
+    setCount(`${newCount}`);
+  }
+
+  return (
+    <div>
+      <h1>Welcome to a distance calculator!</h1>
+      <InputLocation increment={increment} counter={count[0]}/>
+      <Info />
+    </div>
+  );
+}
 
 export default App;
