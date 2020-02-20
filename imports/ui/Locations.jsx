f3c210a2b09a3133a1e3732cdc41507fdae64150
Rebecca Lambert <rebeccalambert@berkeley.edu>
2020-02-19 18:09:05 -0800

    Add CSS reset

--- imports/ui/Locations.jsx
+++ imports/ui/Locations.jsx
@@ -3,16 +3,18 @@ import React from 'react';
 
 function Locations({ maxDistance, closeCities }) {
   let i = 0;
+  let cityList;
   if (closeCities.length === 0) {
-    return (
-      <h1>No cities within that distance! :(</h1>
-    )
+    cityList = 
+      (
+        <h3>No options available</h3>  
+      )      
   } else {
     cityList = closeCities.map( (city) =>{
       i += 1;
       return (
       <li key={i}>
-        <h1>{city.locality}</h1>
+        <h4>{city.street_number} {city.route}, {city.locality}, {city.administrativeArea}, {city.country}, {city.postalCode}</h4>
       </li>
       )  
     })
@@ -20,8 +22,9 @@ function Locations({ maxDistance, closeCities }) {
 
   return (
     <section>
-      <p>You want to stay within {maxDistance} km.</p>
-      <ul> Cities
+      <h2>You want to stay within {maxDistance} km. The addresses in that range are:</h2>
+      {/* <h1>Addresses in that range:</h1> */}
+      <ul> 
         {cityList}
       </ul>
     </section>
